4fdc653cfe31363b7ceba31e1e3258340bfd9d99
Nikkolai Fernandez <nikkolaifernandez@Nikkolais-MacBook-Air.local>
2022-04-25 04:02:21 +0800

    completes user endpoint testing

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -2,22 +2,53 @@ const path = require('path');
 const request = require('supertest')
 const server = require(`${path.resolve('./')}/index.js`);
 
+const signupData = {
+    username: 'johndoe@email.com',
+    password: 'Password#00'
+};
+let jwtToken = null;
+
 afterAll(() => {
     server.close();
 });
 
 describe('User API', () => {
     it('should able to signup', async () => {
-        const data = {
-            username: 'johndoe@email.com',
-            password: 'Password#00'
-        };
         const res = await request(server)
             .post('/signup')
-            .send(data)
+            .send(signupData)
             .set('Accept', 'application/json');
 
+        jwtToken = res.body.token; 
         expect(res.statusCode).toEqual(200)
         expect(res.body).toHaveProperty('token');
+    });
+
+    it('should not able to signup with duplicate email', async () => {
+        const res = await request(server)
+            .post('/signup')
+            .send(signupData)
+            .set('Accept', 'application/json');
+
+        expect(res.statusCode).toEqual(400)
+        expect(res.body).toHaveProperty('errors');
+    });
+
+    it('should able to process user data', async () => {
+        const res = await request(server)
+            .post('/process')
+            .set('Authorization', `Bearer ${jwtToken}`);
+
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('state', true);
+    });
+
+    it('should able to fetch all user datas', async () => {
+        const res = await request(server)
+            .get('/fetch')
+            .set('Authorization', `Bearer ${jwtToken}`);
+
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('datas');
     });
 })
